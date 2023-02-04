07422bfa5484056a78ca76866aa58901e1c8249d
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-04 14:52:51 -0400

    feat: add SideBar options

---
+++ src/views/SideBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-navigation-drawer expand-on-hover rail>
+    <v-list>
+      <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg" title="Sandra Adams"
+        subtitle="sandra_a88@gmailcom"></v-list-item>
+    </v-list>
+    <!-- Divididor -->
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-image" title="Color Galey" value="myfiles"></v-list-item>
+      <v-list-item prepend-icon="mdi-dialpad" title="Cambiar Color Gris" value="shared"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
