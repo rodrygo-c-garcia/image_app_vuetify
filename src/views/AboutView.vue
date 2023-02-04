2ee1152477684c07b2f0043438dab8676c356987
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-04 14:22:17 -0400

    feat: delete components

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,14 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-btn depressed color="primary">
+      Primary
+    </v-btn>
   </div>
 </template>
 
