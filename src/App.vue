b8809c7911bc56b7277d11db900d451dddd76ab6
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-04 14:53:14 -0400

    feat: import SideBar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
+<script setup>
+import SideBar from './views/SideBar.vue';
+</script>
 <template>
   <v-app>
     <!-- Sidebar -->
+    <SideBar />
     <v-main>
-
+      main
     </v-main>
   </v-app>
 </template>
